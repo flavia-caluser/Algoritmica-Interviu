358ff6fbaca7838e052adbf25220cd0498424f65
flavia-caluser <flavia.caluser88@gmail.com>
2024-03-25 11:40:00 +0200

    Add changes

--- src/main/java/org/example/SDA5_Ex1.java
+++ src/main/java/org/example/SDA5_Ex1.java
@@ -1,36 +1,54 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class SDA5_Ex1 {
     public static void main(String[] args) {
-        List<Map<String,Double>> characteristics = List.of(
-                Map.of("temperature", 44.00, "humidity", 12.00),
-                Map.of("temperature", 23.00, "humidity", 34.00),
-                Map.of("temperature", 34.00),
-                Map.of("temperature", null));
+        List<Map<String,Double>> characteristics = new ArrayList<>();
+        Map<String, Double> map1 = new HashMap<>();
+        Map<String, Double> map2 = new HashMap<>();
+        Map<String, Double> map3 = new HashMap<>();
+        Map<String, Double> map4 = new HashMap<>();
+        map1.put("temperature", 44.00);
+        map1.put("humidity", 12.00);
+        map2.put("temperature", 23.00);
+        map2.put("humidity", 34.00);
+        map3.put("temperature", 34.00);
+        map4.put("temperature", null);
+        characteristics.add(map1);
+        characteristics.add(map2);
+        characteristics.add(map3);
+        characteristics.add(map4);
 
+        System.out.println(getAverageCharacteristics(characteristics));
     }
-//MAI TREBUIE SA FAC MEDIA PENTRU FIECARE CHEIE SI AM DOAR SUMELE
     public static Map<String, Double> getAverageCharacteristics(List<Map<String,Double>> characteristicsList){
         Map<String,Double> average = new HashMap<>();
+        Map<String, Integer> counter =new HashMap<>();
         for (int i = 0; i < characteristicsList.size(); i++) {
-            getCharacteristicsFromEachMap(characteristicsList.get(i), average);
+            getCharacteristicsFromEachMap(characteristicsList.get(i), average, counter);
+        }
+        for(String key: average.keySet()){
+            average.put(key, average.get(key)/counter.get(key));
         }
         return average;
     }
 
-    public static void getCharacteristicsFromEachMap(Map<String, Double> map, Map<String, Double> result){
+    public static void getCharacteristicsFromEachMap(Map<String, Double> map, Map<String, Double> result, Map<String, Integer> counter){
         for(Map.Entry<String, Double> entry: map.entrySet()){
             String key = entry.getKey();
             Double value = entry.getValue();
+
             if(value != null){
                 if(!result.containsKey(key)){
                     result.put(key,value);
+                    counter.put(key,1);
                 }else{
                     result.put(key, result.get(key)+ value);
+                    counter.put(key, counter.get(key)+1 );
                 }
             }
         }
